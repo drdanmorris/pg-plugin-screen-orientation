2648ba1e1be9c7597e6fa9f59738bb0d14f8fe00
Daniel Morris <drdanmorris@gmail.com>
2014-02-06 20:15:02 +1300

    Fix invalid js refactoring

--- www/screenOrientation.js
+++ www/screenOrientation.js
@@ -1,5 +1,12 @@
-navigator.screenOrientation = {
-	set: function(str, success, fail) {
-		cordova.exec(null, null, "screenOrientation", "set", [str]);
-	}
+
+var ScreenOrientation = function(){};
+ScreenOrientation.prototype.set = function(str, success, fail) {
+	cordova.exec(null, null, "screenOrientation", "set", [str]);
 };
+
+navigator.screenOrientation = new ScreenOrientation();
+
+if (typeof module != 'undefined' && module.exports) {
+  module.exports = navigator.screenOrientation;
+}
+
