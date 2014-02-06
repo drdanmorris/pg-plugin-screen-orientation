d6e0560f239d61f9e680becfaba7669b5947665c
Daniel Morris <drdanmorris@gmail.com>
2014-02-06 20:53:17 +1300

    JS tweak to correct casing

--- www/screenOrientation.js
+++ www/screenOrientation.js
@@ -1,12 +1,10 @@
 
+
 var ScreenOrientation = function(){};
+
 ScreenOrientation.prototype.set = function(str, success, fail) {
-	cordova.exec(null, null, "screenOrientation", "set", [str]);
+	cordova.exec(null, null, "ScreenOrientation", "set", [str]);
 };
 
-navigator.screenOrientation = new ScreenOrientation();
-
-if (typeof module != 'undefined' && module.exports) {
-  module.exports = navigator.screenOrientation;
-}
+module.exports = new ScreenOrientation();
 
